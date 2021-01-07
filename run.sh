d1bfb3e10895161dd75f1d128348fd9f031502f4
Yunfan Hu <1033718596@qq.com>
2021-01-07 13:28:59 +0800

    resplit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python build_dicts.py
 python build_train.py --processes=10 --max_hist_length 30
-python build_train.py --processes=10 --max_hist_length 30 --fsamples=valid/behaviors.small.tsv
+python build_valid.py --processes=10 --max_hist_length 30 --fsamples=valid/behaviors.small.tsv
 python build_test.py --processes=40 --max_hist_length 30
 python resplit.py --filenum 10
 CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --max_hist_length=30 --epoch=1 --model=ctr_dfm --batch_size=256
